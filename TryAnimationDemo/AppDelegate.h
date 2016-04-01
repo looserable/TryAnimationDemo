4a0692c8670083e7df0080d2d131f95dd1e408bb
lixiaotao <lixiaotao19901103@gmail.com>
2016-04-01 10:05:22 +0800

    Initial Commit

---
+++ TryAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryAnimationDemo
+//
+//  Created by john on 16/4/1.
+//  Copyright © 2016年 jhon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryAnimationDemo
+//
+//  Created by john on 16/4/1.
+//  Copyright © 2016年 jhon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
